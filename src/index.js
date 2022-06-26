8aab5c7a2ceba5e23d0287e26c482707caf2794c
KalyanJyothula <kalyanjyothula@gmail.com>
2022-06-26 20:58:46 +0530

    route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import App from "./container/App/App";
+import App from "./container/App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store/configureStore";
 
